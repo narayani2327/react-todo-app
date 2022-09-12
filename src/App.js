de912f54180a9426a0c640915af4dfd5dea73eed
narayani2327 <harikalanarayani@gmail.com>
2022-09-12 15:03:07 +0530

    basic todo board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import './App.css';
+// import React,{ Component } from 'react';
+// import { TiTick } from 'react-icons/ti';
+// import { RiEdit2Fill, RiDeleteBin5Fill } from 'react-icons/ri'
+// class App extends Component {
+//   render() { 
+//     return (
+//       <>
+//         <a href="" target="_blank"><TiTick/> Phone No.</a>
+//         <a href="" target="_blank"><RiEdit2Fill/> Phone No.</a>
+//         <a href="" target="_blank"><RiDeleteBin5Fill/> Phone No.</a>
+//         <h2>Hello world</h2>
+//         <h3>Hello world</h3>
+//       </>
+//       );
+//   }
+// }
+
+// export default App;
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import  Todo from "./components/Todo";
+import React,{ Component } from 'react';
+class App extends Component {
+  render() { 
+    return (
+      <Todo/>
+      );
+  }
 }
-
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+class Todo extends Component {
+    render() { 
+        return (
+            <div className="todo">
+                <h1>Todo-list</h1>
+            </div>
+        );
+    }
+}
+export default Todo;
